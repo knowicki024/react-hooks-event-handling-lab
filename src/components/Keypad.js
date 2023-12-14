8a414f545082d04661c9e05e8cc37d2dae86f6de
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-12-14 11:05:59 -0500

    lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,21 @@
 // Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+class Keypad extends React.Component{
+    handleChange = () => { 
+        console.log('Entering password...');
+    }
+    render() {
+        return ( 
+            <div>
+                <h1>Enter Password</h1>
+                <input type="password" onChange={this.handleChange} />
+            </div>
+        );
+    }
 }
 
 export default Keypad;
+
+
+
